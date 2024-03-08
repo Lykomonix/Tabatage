99219e4891d23279dfa685e6a0b744a0361fba32
h.hartz <henri.hartz@etu.unistra.fr>
2024-03-08 12:04:38 +0100

    ajout de la page d'accueil

---
+++ code/app/src/main/java/Lykomonix/tabatage/Home.java
@@ -0,0 +1,28 @@
+package Lykomonix.tabatage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Home#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Home extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
