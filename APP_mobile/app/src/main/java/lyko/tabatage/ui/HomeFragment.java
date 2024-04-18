012298696b1c98e9e1f6acce77fdbc04a6937df3
h.hartz <henri.hartz@etu.unistra.fr>
2024-04-18 15:39:35 +0200

    🔨 click lancement application

--- APP_mobile/app/src/main/java/lyko/tabatage/ui/HomeFragment.java
+++ APP_mobile/app/src/main/java/lyko/tabatage/ui/HomeFragment.java
@@ -3,25 +3,43 @@ package lyko.tabatage.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import lyko.tabatage.R;
 
-
 public class HomeFragment extends Fragment {
 
+    private FragmentManager fragmentManager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState == null){
+            fragmentManager = getParentFragmentManager();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button btnStart = view.findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.fcvActivity, ChoiceFragment.class, null);
+                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
     }
 }
