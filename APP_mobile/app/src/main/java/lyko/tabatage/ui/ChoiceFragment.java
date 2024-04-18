dd960a477651f3a8e40fcc060a0d9a398820341a
h.hartz <henri.hartz@etu.unistra.fr>
2024-04-18 16:24:32 +0200

    🔨 condition click vers fragmentAction

--- APP_mobile/app/src/main/java/lyko/tabatage/ui/ChoiceFragment.java
+++ APP_mobile/app/src/main/java/lyko/tabatage/ui/ChoiceFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import lyko.tabatage.R;
 
@@ -32,14 +34,30 @@ public class ChoiceFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_choice, container, false);
         Button btnSubmit = view.findViewById(R.id.btnSubmit);
+
+        TextView txteActionTime = view.findViewById(R.id.txteActionTime);
+        TextView txteRest = view.findViewById(R.id.txteRest);
+        TextView txteRepeat = view.findViewById(R.id.txteRepeat);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.fcvActivity, ActionFragment.class, null);
-                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                String strActionTime = txteActionTime.getText().toString();
+                String strRest = txteRest.getText().toString();
+                String strRepeat = txteRepeat.getText().toString();
+                if(!strActionTime.isEmpty() && !strRest.isEmpty() && !strRepeat.isEmpty()){
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+                    transaction.replace(R.id.fcvActivity, ActionFragment.class, null);
+                    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                }
+                else {
+                    CharSequence text = "vous n'avez pas rempli les champs";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(getContext(), text, duration);
+                    toast.show();
+                }
             }
         });
         return view;
