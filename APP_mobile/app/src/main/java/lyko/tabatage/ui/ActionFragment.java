8685af0d84c7328d9d5d1f9db9ed99116b126bd5
h.hartz <henri.hartz@etu.unistra.fr>
2024-04-18 17:24:16 +0200

    🔨 taffichage des informations du fragment choice

--- APP_mobile/app/src/main/java/lyko/tabatage/ui/ActionFragment.java
+++ APP_mobile/app/src/main/java/lyko/tabatage/ui/ActionFragment.java
@@ -7,10 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import lyko.tabatage.R;
 
 public class ActionFragment extends Fragment {
+
+    String actionTime;
+    String rest;
+    String repeat;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,20 @@ public class ActionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_action, container, false);
+        View view = inflater.inflate(R.layout.fragment_action, container, false);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            actionTime = bundle.getString("actionTime");
+            rest = bundle.getString("rest");
+            repeat = bundle.getString("repeat");
+        }
+
+        TextView txtRepeat = view.findViewById(R.id.txtNbRepeat);
+        TextView txtTime = view.findViewById(R.id.txtTime);
+
+        txtRepeat.setText(String.format("0 / %s", repeat));
+        txtTime.setText(String.format("%s s",actionTime));
+
+        return view;
     }
 }
