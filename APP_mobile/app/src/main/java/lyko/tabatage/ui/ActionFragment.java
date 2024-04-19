24fba1182e4b5ceae579f96da97bd70268f2eaed
h.hartz <henri.hartz@etu.unistra.fr>
2024-04-19 11:38:49 +0200

    🔨 ajout d'un timer

--- APP_mobile/app/src/main/java/lyko/tabatage/ui/ActionFragment.java
+++ APP_mobile/app/src/main/java/lyko/tabatage/ui/ActionFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import lyko.tabatage.R;
 
@@ -16,6 +20,11 @@ public class ActionFragment extends Fragment {
     String actionTime;
     String rest;
     String repeat;
+    int nbrepeat;
+
+    public ActionFragment(){
+        nbrepeat = 0;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,31 @@ public class ActionFragment extends Fragment {
 
         txtRepeat.setText(String.format("0 / %s", repeat));
         txtTime.setText(String.format("%s s",actionTime));
+
+        long timerDuration = TimeUnit.MINUTES.toMillis(1);
+        long ticksInterval = 10;
+
+        new CountDownTimer(timerDuration, ticksInterval) {
+            long millis = 1000;
+            @Override
+            public void onTick(long millisUntilFinished) {
+                millis = millis - ticksInterval;
+                if (millis == 0){
+                    millis = 1000;
+                    txtRepeat.setText(String.format("%s / %s", nbrepeat, repeat));
+                }
+                String timerText = String.format(Locale.getDefault(),"%02d:%02d:%02d",
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished));
+                txtTime.setText(timerText);
+            }
+
+            @Override
+            public void onFinish() {
+                txtTime.setText("finished");
+            }
+        }.start();
 
         return view;
     }
